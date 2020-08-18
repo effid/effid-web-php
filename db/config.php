ce6079c5c52e593a8538f37c7f4a3da46ffda850
Skyrule8 <lthorel@efficom-lille.com>
2020-08-18 18:01:07 +0200

    Modif login

--- db/config.php
+++ db/config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'effid');
+//define('DB_SERVER', 'localhost');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', '');
+//define('DB_NAME', 'effid');
  
 // Connexion à la base de données MySQL 
 //$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
